6a062c5146388230420c9e8e122eb572e5e587af
Brave Trinh <utachi68@gmail.com>
2018-07-30 17:59:09 +0700

    update delete dialog

---
+++ app/code/BBApps/Checkout/view/frontend/requirejs-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {{map: {"*": {discountCode: string, shoppingCart: string, regionUpdater: string, sidebar: string, checkoutLoader: string, checkoutData: string, proceedToCheckout: string}}}}
+ */
+var config = {
+    map: {
+        '*': {
+            custom_sidebar: 'BBApps_Checkout/js/custom_sidebar'
+        }
+    }
+};
