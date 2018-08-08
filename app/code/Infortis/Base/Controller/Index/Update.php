69fecabaeb1f97c6fb3fdf4555d93c88425b3a9c
Konstantin Zima <zimakm@gmail.com>
2018-08-08 20:07:22 +0300

    Fix update quantity of items in section Extra Products when increase/decrease Extra Products items in cart at checkout.

--- app/code/Infortis/Base/Controller/Index/Update.php
+++ app/code/Infortis/Base/Controller/Index/Update.php
@@ -1,6 +1,10 @@
 <?php
+
 namespace Infortis\Base\Controller\Index;
+
 use Magento\Framework\Controller\ResultFactory;
+
+
 class Update extends \Magento\Checkout\Controller\Cart
 {
     public function execute()
@@ -9,17 +13,20 @@ class Update extends \Magento\Checkout\Controller\Cart
 		$itemId = $this->getRequest()->getParam('itemid'); 
 		$qty 	= $this->getRequest()->getParam('qty'); 
 		$quoteItem = $this->cart->getQuote()->getItemById($itemId);
+
 		if ($quoteItem->getId()) {
 			$quoteItem->setQty((double) $qty);
 			$quoteItem->getProduct()->setIsSuperMode(true);
 			$quoteItem->save(); 
 			$this->cart->getQuote()->collectTotals();
 		}
+
 		$extrasProduct = $objectManager->get('Infortis\Base\Helper\Data')->getExtrasProduct();
 		$response['status'] = true;
 		$response['extras'] = count($extrasProduct) > 0 ? $extrasProduct : false;
 		$resultJson = $this->resultFactory->create(ResultFactory::TYPE_JSON);
 		$resultJson->setData($response);
+
         return $resultJson;
     }
 }
